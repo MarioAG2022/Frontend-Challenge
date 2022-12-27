697d86111904810188ef99b116f1e94b6bac6c85
MarioAG2022 <correoaguilera2@gmail.com>
2022-12-27 16:59:47 -0600

    Se agrego el ccs del contenedor de los productos

--- src/componentes/Producto.js
+++ src/componentes/Producto.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import '../css/Producto.css';
 import App from '../App';
 
 function Producto(){
   return(
     <div className='contenedor-producto'>
-      <img 
-      className='imagen-producto' 
-      src={require('../img/img-cama.png')}
-      alt='Icono de cama'
-      />
-      <div className='Contenedor-texto-nombre'>
-        <p className='nombre-producto'>Beds</p>
+      <div className='contenedor-iconos'>
+        <img 
+        className='imagen-producto' 
+        src={require('../img/img-cama.png')}
+        alt='Icono de cama'
+        />
+        <br></br>
+        <div className='Contenedor-texto-nombre'>
+          <p className='nombre-producto'>Beds</p>
+        </div>
       </div>
     </div> 
   );
