8ea589d038ade17c6468830b29f1b34985c9a893
MarioAG2022 <correoaguilera2@gmail.com>
2022-12-27 23:15:18 -0600

    Se agregraron los estados de cada producto

--- src/componentes/Producto.js
+++ src/componentes/Producto.js
@@ -1,37 +1,57 @@
 import React from 'react';
 import '../css/Producto.css';
+import '../css/Boton.css';
+import '../css/Contador.css';
 import Boton from './Boton';
-import App from '../App';
+import Contador from './Contador';
+import { useState } from 'react';
 
 function Producto(props){
+  const [numClics,setNumClics] = useState(0); 
   const manejarClic = () =>{
-    console.log('Mas');
+    setNumClics(numClics + 1);
   }
   const manejarClicMenos = () =>{
-    console.log('Menos');
+    setNumClics(numClics - 1);
+  }
+  const manejarClicClean = () =>{
+    setNumClics(0);
   }
   
   return(
       <div className='contenedor-iconos'>
+        
         <img 
-        className='imagen-producto' 
-        src={require(`../img/img-${props.imagen}.png`)}
-        alt='Icono de cama'
+          className='imagen-producto' 
+          src={require(`../img/img-${props.imagen}.png`)}
+          alt='Icono de cama'
         />
         <div className='contenedor-texto-nombre'>
           <p className='nombre-producto'>{props.nombre}</p>
         </div>
-        <Boton
-            texto='Clic'
+        <div className='contenedor-contador'>
+          <Boton
+            className='operador'
+            texto='+'
             esBotonDeMas={true}
             manejarClic={manejarClic}
-            />
+          />
+          <Contador numClics={numClics}/>
           <Boton
-            texto='Menos'
+            className='operador'
+            texto='-'
             esBotonDeMas={false}
             manejarClic={manejarClicMenos}
           />
+          </div>
+          <Boton
+            className='limpiador'
+            texto='Clear'
+            esBotonDeMas={false}
+            manejarClic={manejarClicClean}
+            />
       </div>
+        
   );
 }
 
